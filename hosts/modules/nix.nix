574fd975c1ab3079f95ba3ab1b994f97538c477b
Philipp Jungkamp <p.jungkamp@gmx.net>
2023-02-22 12:33:14 +0100

    use system nixpkgs for nix flake registry

--- hosts/modules/nix.nix
+++ hosts/modules/nix.nix
@@ -1,10 +1,13 @@
-{...}: {
+{self, ...}: {
   config = {
     # allow unfree packages
     nixpkgs.config.allowUnfree = true;
 
     # enable flakes
     nix.settings.experimental-features = ["nix-command" "flakes"];
+
+    # use system nixpkgs for flake registry
+    nix.registry.nixpkgs.flake = self.inputs.nixpkgs;
 
     # use nix-index to find packages for missing commands
     programs.nix-index.enable = true;
